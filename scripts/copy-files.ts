0b865da0c83e2ebaa4e6196bdbc071c1a928e436
Tiago Peres França <tiago.franca@zup.com.br>
2023-08-16 09:31:45 -0300

    changeset

---
+++ scripts/copy-files.ts
@@ -0,0 +1,24 @@
+/**
+ * This copies the readme and license files to every package before publishing them.
+ */
+import { copyFile } from 'fs/promises'
+
+async function copy(pckg: string) {
+  await Promise.all([
+    copyFile('./LICENSE.txt', `./packages/${pckg}/out/LICENSE.txt`),
+    copyFile('./README.md', `./packages/${pckg}/out/README.md`),
+  ])
+}
+
+async function start() {
+  try {
+    await Promise.all(['backend', 'client', 'react', 'shared'].map(copy))
+    process.exit(0)
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error)
+    process.exit(1)
+  }
+}
+
+start()

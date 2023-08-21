13a3b4f6321f38f40cc03e35b1cd22cafd6d7076
Tiago Peres França <tiago.franca@zup.com.br>
2023-08-21 11:22:57 -0300

    tests for VSCodeWebviewBridge

--- packages/backend/test/VSCodeWebviewBridge.spec.ts
+++ packages/backend/test/VSCodeWebviewBridge.spec.ts
@@ -1,16 +1,42 @@
+import { Webview } from 'vscode'
+import { MessageHandler } from '../src/MessageHandler'
+import { VSCodeWebviewBridge } from '../src/VSCodeWebviewBridge'
+
+jest.mock('../src/MessageHandler')
+
+class Bridge extends VSCodeWebviewBridge<{ test: string, hello: string }> {}
+
+function getMessageHandlerInstance(): MessageHandler {
+  return (MessageHandler as jest.Mock).mock.instances[0]
+}
+
 describe('VSCodeWebviewBridge', () => {
+  const webview: Webview = {
+    asWebviewUri: jest.fn(),
+    cspSource: '',
+    html: '',
+    onDidReceiveMessage: jest.fn(),
+    postMessage: jest.fn(),
+    options: {},
+  }
+  const bridge = new Bridge(webview)
+  
   it('should build message handler', () => {
-
+    expect(MessageHandler).toBeCalled()
   })
-  it('should get state', () => {
 
+  it('should get state', () => {
+    bridge.state.get('test')
+    expect(getMessageHandlerInstance().getState).toBeCalledWith('test')
   })
 
   it('should set state', () => {
-    
+    bridge.state.set('hello', 'world')
+    expect(getMessageHandlerInstance().setState).toBeCalledWith('hello', 'world')
   })
 
   it('should dispose', () => {
-
+    bridge.dispose()
+    expect(getMessageHandlerInstance().dispose).toBeCalled()
   })
 })
